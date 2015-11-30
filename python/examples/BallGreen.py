f16ef41cf163d982c5e98105c9622210e4936997
plasmastrike <mattplasmastrike@gmail.com>
2015-11-30 16:21:29 -0700

    Changed to the correct number ol leds

--- python/examples/BallGreen.py
+++ python/examples/BallGreen.py
@@ -18,36 +18,12 @@ LED_INVERT     = False   # True to invert the signal (when using NPN transistor 
 
 
 # Define functions which animate LEDs in various ways.
-
-
-def wheel(pos):
-	"""Generate rainbow colors across 0-255 positions."""
-	if pos < 85:
-		return Color(pos * 3, 255 - pos * 3, 0)
-	elif pos < 170:
-		pos -= 85
-		return Color(255 - pos * 3, 0, pos * 3)
-	else:
-		pos -= 170
-		return Color(0, pos * 3, 255 - pos * 3)
-
-def rainbow(strip, wait_ms=20, iterations=1):
-	"""Draw rainbow that fades across all pixels at once."""
-	for j in range(256*iterations):
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel((i+j) & 255))
+def colorWipe(strip, color, wait_ms=50):
+	"""Wipe color across display a pixel at a time."""
+	for i in range(strip.numPixels()):
+		strip.setPixelColor(i, color)
 		strip.show()
 		time.sleep(wait_ms/1000.0)
-
-def rainbowCycle(strip, wait_ms=20, iterations=5):
-	"""Draw rainbow that uniformly distributes itself across all pixels."""
-	for j in range(256*iterations):
-		for i in range(strip.numPixels()):
-			strip.setPixelColor(i, wheel(((i * 256 / strip.numPixels()) + j) & 255))
-		strip.show()
-		time.sleep(wait_ms/1000.0)
-
-
 
 
 # Main program logic follows:
@@ -59,6 +35,6 @@ if __name__ == '__main__':
 
 	print 'Press Ctrl-C to quit.'
 	while True:
-		# Rainbow animations.
-		#rainbow(strip)
-		rainbowCycle(strip)
+		colorWipe(strip, Color(0, 255, 154))# Blue Wipe
+                colorWipe(strip, Color(0, 0 , 0))
+                
